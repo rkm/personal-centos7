0dd93811401a6eddee336e4e2e6211700fe5a879
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-12-28 16:04:59 +0000

    Fix install script

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 set -ev
 
 sudo yum -y install git epel-release ansible
+wget -q -O - https://raw.githubusercontent.com/rkm/personal-centos7-ansible/master/requirements.yml
 ansible-galaxy install -r requirements.yml
 ansible-pull -U https://github.com/rkm/personal-centos7-ansible
