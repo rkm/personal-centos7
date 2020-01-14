90e258ca91e9ea5acb3e63054136f9e4438b5708
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2020-01-14 00:35:55 +0000

    [skip ci] Sssshhhhh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-sudo yum -y install epel-release && sudo yum -y install git ansible
+sudo yum -q -y install epel-release && sudo yum -q -y install git ansible
 git clone --depth=1 https://github.com/rkm/personal-centos7-ansible
 cd personal-centos7-ansible
 ansible-galaxy install -r requirements.yml
